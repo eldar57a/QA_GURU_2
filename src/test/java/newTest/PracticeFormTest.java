27aca994f78e52fd30de3822a8a956669eefe37c
Akhnazarov Eldar <eldarorel57@gmail.com>
2026-01-06 19:22:45 +0300

    rename PracticeFormTest

--- src/test/java/newTest/PracticeFormTest.java
+++ src/test/java/newTest/PracticeFormTest.java
@@ -1,17 +1,10 @@
 package newTest;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.PracticeFormRegistrationPage;
 import pages.components.ResultsTableComponent;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-
-public class PracticeForm extends TestBase {
+public class PracticeFormTest extends TestBase {
 
     PracticeFormRegistrationPage practiceFormRegistrationPage = new PracticeFormRegistrationPage();
     ResultsTableComponent resultsTableComponent = new ResultsTableComponent();
